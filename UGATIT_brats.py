6c7ebc6c8149f38a1d6fe337d64423e3d097b781
chenxu31 <chenxu31@gmail.com>
2023-10-14 23:07:33 +0800

    sync

--- UGATIT_brats.py
+++ UGATIT_brats.py
@@ -188,8 +188,8 @@ class UGATIT(object) :
                     self.G_optim.param_groups[0]['lr'] -= (self.lr / (self.iteration // 2))
                     self.D_optim.param_groups[0]['lr'] -= (self.lr / (self.iteration // 2))
 
-                real_A = data_s["image"].to(device)
-                real_B = data_t["image"].to(device)
+                real_A = data_s["image"].to(self.device)
+                real_B = data_t["image"].to(self.device)
                 """
                 try:
                     real_A, _ = trainA_iter.next()
